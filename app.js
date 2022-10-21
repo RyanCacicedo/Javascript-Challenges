da4139b422bfe43a532fa5a4efaac1590a46624c
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-21 16:55:49 -0400

    Last beginner challenges

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
-function oppositeBoolean(bool) {
-  return !bool;
-}
+function isLoggedInAndSubscribed(LoggedIn, Subscribed) {
+    return (LoggedIn === `LOGGEDIN`) && (Subscribed === `SUBSCRIBED`)
+   }
+   console.log(isLoggedInAndSubscribed(`LOGGEDIN`, `SUBSCRIBED`))
 
-console.log(oppositeBoolean(false));
-
-function isNotZero(num) {
-  return num !== 0;
-}
-
-console.log(isNotZero(5));
-
-function calcRemainder(num1, num2) {
-  return num1 % num2;
-}
-
-console.log(calcRemainder(7,8));
-
-function isOdd(num) {
-  return num % 2 !== 0
-}
-
-console.log(isOdd(7));
-
-function isEven(num) {
- if (num % 2 === 0){
-     return 1
-    }
-    return -1
-}
-
-console.log(isEven(12));
-//Shorter Version
-
-function isEvenn(num) {
- return num % 2 === 0 ? 1 : -1
-}
-console.log(isEvenn(3))
+function isLoggedInAndSubscribed(LoggedIn, Subscribed) {
+    return (LoggedIn === `LOGGEDIN`) || (Subscribed === `SUBSCRIBED`)
+   }
+   console.log(isLoggedInAndSubscribed(`LOGGEDIN`, `UNSUBSCRIBED`))
