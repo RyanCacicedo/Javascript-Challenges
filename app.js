665ba5c038204c8bf1b06920728763c0e76acffc
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-21 15:39:48 -0400

    First 3 beginner challenges

---
+++ app.js
@@ -0,0 +1,18 @@
+function addition(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addition(3, 5))
+
+function ConvertHoursIntoSeconds(hours) {
+    return hours * 1800
+}
+
+console.log(ConvertHoursIntoSeconds(2))
+
+
+function perimeterOfRectangle(length, width) {
+    return 2 * length + 2 * width
+}
+
+console.log(perimeterOfRectangle(6,7))
