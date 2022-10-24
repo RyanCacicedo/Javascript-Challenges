63f3c1dc2aa04e286be5742db669ea15d6e26c17
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-24 16:25:54 -0400

    first medium javascript challenges

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-function isLoggedInAndSubscribed(LoggedIn, Subscribed) {
-    return (LoggedIn === `LOGGEDIN`) && (Subscribed === `SUBSCRIBED`)
-   }
-   console.log(isLoggedInAndSubscribed(`LOGGEDIN`, `SUBSCRIBED`))
+function falsyOrTruthy(elem1, elem2) {
+    return (!elem1) ? elem1 : elem2;
+}
 
-function isLoggedInAndSubscribed(LoggedIn, Subscribed) {
-    return (LoggedIn === `LOGGEDIN`) || (Subscribed === `SUBSCRIBED`)
-   }
-   console.log(isLoggedInAndSubscribed(`LOGGEDIN`, `UNSUBSCRIBED`))
+console.log(falsyOrTruthy(0,5))
+
+
+function arrLength(arr) {
+    return arr.length
+}
+
+console.log(arrLength([1, 2, 3, 4]))
+
+
+function arrLength(arr) {
+    return arr[arr.length - 1];
+}
+
+console.log(arrLength([1, 2, 3, 4, 50]))
+
+
+function arrSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; ++i) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+console.log(arrSum([2, 2, 2]))
+
+
+function progressiveSum(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; ++i) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+console.log(progressiveSum([4]))
