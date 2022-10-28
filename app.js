29320e20f059aa4d1c3ea4da7216c61eae82dffa
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-28 13:55:45 -0400

    More medium challenges

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-function falsyOrTruthy(elem1, elem2) {
-    return (!elem1) ? elem1 : elem2;
+function calcTime(seconds) {
+   let timerMinutes = Math.floor(seconds / 60)
+   let timerSeconds = seconds % 60;
+
+   if (timerMinutes.toString().length === 1){
+    timerMinutes = `0` + timerMinutes
+   }
+
+   return timerMinutes + `:` +timerSeconds
 }
 
-console.log(falsyOrTruthy(0,5))
+console.log(calcTime([70]))
 
 
-function arrLength(arr) {
-    return arr.length
-}
-
-console.log(arrLength([1, 2, 3, 4]))
-
-
-function arrLength(arr) {
-    return arr[arr.length - 1];
-}
-
-console.log(arrLength([1, 2, 3, 4, 50]))
-
-
-function arrSum(arr) {
-    let sum = 0;
+function getMax(arr) {
+   let max = arr[0]
     for (let i = 0; i < arr.length; ++i) {
-        sum = sum + arr[i];
+    if (arr[i] > max) {
+        max = arr[i];
     }
-    return sum;
+   }
+   return max;
+}
+console.log(getMax([10, -200, -300]))
+
+// incrementing for Loop
+function reverseString(str) {
+    let reversedString = ``
+    for (let i = 0; i < str.length; ++i) {
+   reversedString = str[i] + reversedString
+   }
+   return reversedString;
+}
+console.log(reverseString(`abc`))
+
+//decrementing for Loop
+function reverseString(str) {
+    let reversedString = ``
+    for (let i = str.length - 1; i >= 0; --i) {
+   reversedString += str[i]
+   }
+   return reversedString;
+}
+console.log(reverseString(`abc`))
+
+//Array reverse property
+function reverseString(str) {
+    return str.split(``).reverse().join(``);
 }
 
-console.log(arrSum([2, 2, 2]))
-
-
-function progressiveSum(num) {
-    let sum = 0;
-    for (let i = 1; i <= num; ++i) {
-        sum = sum + i;
-    }
-    return sum;
-}
-
-console.log(progressiveSum([4]))
+console.log(reverseString(`Ryan`))
