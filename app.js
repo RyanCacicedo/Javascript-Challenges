fa13c3afd526e70a326f7a0924053cc0fafc1da0
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-31 14:27:59 -0400

    Last medium challenges

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
-//Turn everything to zeroes
-
-//For loop method
-function convertToZeros(arr) {
-    let newArr = []
-    for (let i = 0; i < arr.length; ++i) {
-        newArr[i] = 0
-    }
-    return newArr
-}
-console.log(convertToZeros([1, 2, 3, 4, 5, 6]))
-
-//Array fill method
-function convertToZeros(arr) {
-    return new Array(arr.length).fill(0)
-}
-console.log(convertToZeros([1, 2, 3, 4, 5, 6]))
-
-//array.map method
-function convertToZeros(arr) {
-    return arr.map(elem => 0)
-}
-console.log(convertToZeros([1, 2, 3, 4, 5, 6]))
-
-//filter out apples for method
-function removeApples(arr) {
-    let noApples = []
-    for (let i = 0; i < arr.length; ++i) {
-        if (arr[i] !== `Apple`) {
-            noApples.push(arr[i]);
-        }
-    }
-    return noApples
-}
-console.log(removeApples(['bananna', 'Apple', 'orange']))
-
-//filter out apples array.filter method
-function removeApples(arr) {
-    return arr.filter(elem => elem !== `Apple`)
+//filter out falsy values
+function filterOutFalsy(arr) {
+    return arr.filter(elem => !!elem === true)
 }
 
-console.log(removeApples([`bananna`, `Apple`, "Orange"]))
+console.log(filterOutFalsy([``, NaN, 500, null, false, 0]))
+
+//convert to Boolean
+function convertToBoolean(arr) {
+    return arr.map(elem => !!elem)
+}
+
+console.log(convertToBoolean([``, NaN, false, null, false, 0]))
