78643ffc5460a853ba7eba8dcce0642a09cb5a75
RyanCacicedo <Ryancacicedo@gmail.com>
2022-11-18 13:23:02 -0500

    Advanced challenges

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-//filter out falsy values
-function filterOutFalsy(arr) {
-    return arr.filter(elem => !!elem === true)
+function showRating(rating) {
+    let ratings = "";
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratings += "*";
+        if (i !== Math.floor(rating) - 1) {
+            ratings += " ";
+        }
+    }
+    if (!Number.isInteger(rating)) {
+        ratings += " .";
+    }
+    return ratings;
 }
 
-console.log(filterOutFalsy([``, NaN, 500, null, false, 0]))
+console.log(showRating(4.5))
 
-//convert to Boolean
-function convertToBoolean(arr) {
-    return arr.map(elem => !!elem)
-}
+// Directions for problem above
 
-console.log(convertToBoolean([``, NaN, false, null, false, 0]))
+// initialise an empty string
+
+// loop through the rounded down rating
+
+    // add a star for every iteration 
+
+    // if its not the last iteration add a space
+
+// if the number is not an integer
+
+    // add a full stop
+
+// return it
