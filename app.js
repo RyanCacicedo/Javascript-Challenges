c2ab1ee5c1c507516bbb215f6ad24f1ddec272a0
RyanCacicedo <Ryancacicedo@gmail.com>
2022-11-18 13:33:15 -0500

    Sort numbers low to high

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-function showRating(rating) {
-    let ratings = "";
-    for (let i = 0; i < Math.floor(rating); ++i) {
-        ratings += "*";
-        if (i !== Math.floor(rating) - 1) {
-            ratings += " ";
-        }
-    }
-    if (!Number.isInteger(rating)) {
-        ratings += " .";
-    }
-    return ratings;
+function sortLowToHigh(numbers) {
+    return numbers.sort((a,b) => a-b);
 }
 
-console.log(showRating(4.5))
-
-// Directions for problem above
-
-// initialise an empty string
-
-// loop through the rounded down rating
-
-    // add a star for every iteration 
-
-    // if its not the last iteration add a space
-
-// if the number is not an integer
-
-    // add a full stop
-
-// return it
+console.log(sortLowToHigh([1, 5, 0, 10, 4, 10567]))
